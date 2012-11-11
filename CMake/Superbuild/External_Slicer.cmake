b6d5409d79e727b579d054c8d95ca38f2a5b0361
Julien Finet <julien.finet@kitware.com>
2012-11-11 16:23:13 -0500

    Add Superbuild, download, configure and build Slicer
    Issue #13632

---
+++ CMake/Superbuild/External_Slicer.cmake
@@ -0,0 +1,99 @@
+#============================================================================
+#
+# Program: Bender
+#
+# Copyright (c) Kitware Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#============================================================================
+
+#
+# Slicer
+#
+set(proj Slicer)
+
+# Make sure this file is included only once
+get_filename_component(proj_filename ${CMAKE_CURRENT_LIST_FILE} NAME_WE)
+if(${proj_filename}_proj)
+  return()
+endif()
+set(${proj_filename}_proj ${proj})
+
+# Sanity checks
+if(DEFINED ${proj}_DIR AND NOT EXISTS ${${proj}_DIR})
+  message(FATAL_ERROR "${proj}_DIR variable is defined but corresponds to non-existing directory")
+endif()
+
+# Set dependency list
+set(${proj}_DEPENDENCIES BenderLibs)
+
+# Include dependent projects if any
+benderMacroCheckExternalProjectDependency(${proj})
+
+# Restore the proj variable
+get_filename_component(proj_filename ${CMAKE_CURRENT_LIST_FILE} NAME_WE)
+set(proj ${${proj_filename}_proj})
+
+if(NOT DEFINED ${proj}_DIR)
+  message(STATUS "${__indent}Adding project ${proj}")
+  find_package(Qt4 REQUIRED)
+
+  # Set CMake OSX variable to pass down the external project
+  set(CMAKE_OSX_EXTERNAL_PROJECT_ARGS)
+  if(APPLE)
+    list(APPEND CMAKE_OSX_EXTERNAL_PROJECT_ARGS
+      -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
+      -DCMAKE_OSX_SYSROOT=${CMAKE_OSX_SYSROOT}
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
+  endif()
+
+  set(${proj}_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
+  ExternalProject_Add(${proj}
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
+    BINARY_DIR ${${proj}_DIR}
+    PREFIX ${proj}${ep_suffix}
+    GIT_REPOSITORY "${git_protocol}://github.com/Slicer/Slicer.git"
+    GIT_TAG "f732a19e5b80d7fc8fb8b304f0e2a6162ceb9f92"
+    UPDATE_COMMAND ""
+    INSTALL_COMMAND ""
+    CMAKE_GENERATOR ${gen}
+    CMAKE_ARGS
+      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+      -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
+      -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
+      -DCMAKE_INSTALL_PREFIX:PATH=${ep_install_dir}
+      ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}
+      -DADDITIONAL_C_FLAGS:STRING=${ADDITIONAL_C_FLAGS}
+      -DADDITIONAL_CXX_FLAGS:STRING=${ADDITIONAL_CXX_FLAGS}
+      -DBUILD_TESTING:BOOL=OFF
+      -D${proj}_INSTALL_BIN_DIR:STRING=${BENDER_INSTALL_BIN_DIR}
+      -D${proj_NAME}_INSTALL_LIB_DIR:STRING=${BENDER_INSTALL_BIN_DIR}
+      -DGIT_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}
+      -D${proj_NAME}_USE_GIT_PROTOCOL:BOOL=${BENDER_USE_GIT_PROTOCOL}
+      -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE}
+      -DSlicer_REQUIRED_QT_VERSION:STRING=${QT_VERSION_MAJOR}.${QT_VERSION_MINOR}.${QT_VERSION_PATCH}
+      -DSlicer_EXTENSION_SOURCE_DIRS:STRING=${Bender_SOURCE_DIR}/Modules/Loadable/Armatures
+    INSTALL_COMMAND ""
+    DEPENDS
+      ${${proj}_DEPENDENCIES}
+    )
+
+else()
+  # The project is provided using ${proj}_DIR, nevertheless since other project may depend on ${proj},
+  # let's add an 'empty' one
+  #benderMacroEmptyExternalProject(${proj} "${${proj}_DEPENDENCIES}")
+endif()
+
+list(APPEND Bender_SUPERBUILD_EP_ARGS -${proj}_DIR:PATH=${${proj}_DIR})
+
